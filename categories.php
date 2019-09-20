83f7fc452834e7f842c0527a8a40c1588dfd1f69
Godspower <gpanyaog@gmail.com>
2019-09-20 13:07:13 +0100

    Fixed scroll

--- categories.php
+++ categories.php
@@ -9,54 +9,61 @@
 </head>
 <body>
     <section class="container-fluid" id="section">
-        <div class="d-flex" id="flex-categories">
+        <div id="flex-categories">
                 <div class="card m-1">
                     <img src="./assets/logo.png" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-title font-weight-bold">Food</p>
-                        <p class="card-text ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odio tenetur quisquam, accusamus deserunt animi vitae ex dolore recusandae saepe maiores excepturi totam adipisci libero iusto suscipit eius. Possimus, laboriosam!</p>
+                        <p class="card-text ">Checkout our Food stores !</p>
+                        <button class="btn btn-primary">Checkout</button>
                     </div>
                 </div>
                 <div class="card m-1">
                     <img src="./assets/logo.png" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-title font-weight-bold">Hotels</p>
-                        <p class="card-text ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odio tenetur quisquam, accusamus deserunt animi vitae ex dolore recusandae saepe maiores excepturi totam adipisci libero iusto suscipit eius. Possimus, laboriosam!</p>
+                        <p class="card-text ">Checkout our Hotels !</p>
+                        <button class="btn btn-primary">Checkout</button>
                     </div>
                 </div>
                 <div class="card m-1">
                     <img src="./assets/logo.png" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-title font-weight-bold">Bar</p>
-                        <p class="card-text ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odio tenetur quisquam, accusamus deserunt animi vitae ex dolore recusandae saepe maiores excepturi totam adipisci libero iusto suscipit eius. Possimus, laboriosam!</p>
+                        <p class="card-text ">Checkout our Bars !</p>
+                        <button class="btn btn-primary">Checkout</button>
                     </div>
                 </div>
                 <div class="card m-1">
                     <img src="./assets/logo.png" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-title font-weight-bold">Food</p>
-                        <p class="card-text ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odio tenetur quisquam, accusamus deserunt animi vitae ex dolore recusandae saepe maiores excepturi totam adipisci libero iusto suscipit eius. Possimus, laboriosam!</p>
+                        <p class="card-text ">Checkout our Food stores !</p>
+                        <button class="btn btn-primary">Checkout</button>
                     </div>
                 </div>
                 <div class="card m-1">
                     <img src="./assets/logo.png" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-title font-weight-bold">Police Station</p>
-                        <p class="card-text ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odio tenetur quisquam, accusamus deserunt animi vitae ex dolore recusandae saepe maiores excepturi totam adipisci libero iusto suscipit eius. Possimus, laboriosam!</p>
+                        <p class="card-text ">Checkout our Security buildings !</p>
+                        <button class="btn btn-primary">Checkout</button>
                     </div>
                 </div>
                 <div class="card m-1">
                     <img src="./assets/logo.png" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-title font-weight-bold">Beach</p>
-                        <p class="card-text ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odio tenetur quisquam, accusamus deserunt animi vitae ex dolore recusandae saepe maiores excepturi totam adipisci libero iusto suscipit eius. Possimus, laboriosam!</p>
+                        <p class="card-text ">Checkout our Beach !</p>
+                        <button class="btn btn-primary">Checkout</button>
                     </div>
                 </div>
                 <div class="card m-1">
                     <img src="./assets/logo.png" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-title font-weight-bold">Car Park</p>
-                        <p class="card-text ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odio tenetur quisquam, accusamus deserunt animi vitae ex dolore recusandae saepe maiores excepturi totam adipisci libero iusto suscipit eius. Possimus, laboriosam!</p>
+                        <p class="card-text ">Checkout our Parks !</p>
+                        <button class="btn btn-primary">Checkout</button>
                     </div>
                 </div>
             </div>
